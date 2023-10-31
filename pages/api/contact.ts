143a5f8b2a19d33f4c4ad44bd888cfc3a5fa10fd
Harry <harryyy27@gmail.com>
2023-10-31 10:54:34 +0000

    where did it come from?

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -4,22 +4,28 @@ import { getCsrfToken } from 'next-auth/react'
 import { sendEmail } from '../../utils/nodemailer'
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
     try{
+        console.log('this bit works')
         if(req.method==='POST'){
             if(!req.headers.csrftoken){
                 throw new Error('No csrf header found.')
             }
+            console.log('and this')
             const csrftoken = await getCsrfToken({req:{headers:req.headers}})
             if(req.headers.csrftoken!==csrftoken){
                 throw new Error('CSRF authentication failed.')
             }
             
+            console.log('and this too')
             const body = JSON.parse(req.body);
+
+            console.log('going strong')
             await sendEmail({
                         subject: `Email from ${body.name}`,
                         text:body.textContent,
                         to:process.env.COMPANY_EMAIL,
                         from:body.email
             })
+            console.log('really?')
             res.status(200).json({success: true})
         }
         else {
@@ -28,6 +34,7 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
     }
     catch(e:any){
         console.log(e)
+        console.log('first error here')
         await errorHandler(JSON.stringify(req.headers),JSON.stringify(req.body),req.method as string,e.message,e.stack,false)
         res.status(500).json({
             success:false
