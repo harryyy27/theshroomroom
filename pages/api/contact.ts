ac77442fc1dfd846cd8c6ca4ba0e728fa5cb99a6
Harry <harryyy27@gmail.com>
2023-10-31 12:39:48 +0000

    register logs

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -9,23 +9,19 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
             if(!req.headers.csrftoken){
                 throw new Error('No csrf header found.')
             }
-            console.log('and this')
             const csrftoken = await getCsrfToken({req:{headers:req.headers}})
             if(req.headers.csrftoken!==csrftoken){
                 throw new Error('CSRF authentication failed.')
             }
             
-            console.log('and this too')
             const body = JSON.parse(req.body);
 
-            console.log('going strong')
             await sendEmail({
                         subject: `Email from ${body.name}`,
                         text:body.textContent,
                         to:process.env.COMPANY_EMAIL,
                         from:body.email
             })
-            console.log('really?')
             res.status(200).json({success: true})
         }
         else {
@@ -34,7 +30,6 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
     }
     catch(e:any){
         console.log(e)
-        console.log('first error here')
         await errorHandler(JSON.stringify(req.headers),JSON.stringify(req.body),req.method as string,e.message,e.stack,false)
         res.status(500).json({
             success:false
