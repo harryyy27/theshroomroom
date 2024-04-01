364a830963b751de28349184c2513d706f419a7e
Harry <harryyy27@gmail.com>
2024-04-01 17:23:41 +0100

    postcodes added to delivery page

--- pages/delivery.tsx
+++ pages/delivery.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {Metadata} from '../utils/metadata/metadata';
-
+import ZedPostcodes from '../utils/zedPostcodes/postcodes'
 export default function Delivery(){
     return (
         <div className="static-container">
@@ -14,12 +14,101 @@ export default function Delivery(){
         <section>
             <h2>Mushies</h2>
             <p>
-                For the time being we will be limiting the delivery of fresh mushrooms to the postcodes listed at the bottom of this page. However, dried mushrooms and seasoning can be delivered to anywhere in the country.
+                For the time being we will be limiting the delivery of fresh mushrooms to the postcodes listed at the bottom of this page. However, dried mushrooms and seasoning can be delivered to anywhere in the country. Deliveries will arrive in 1-4 working days.
             </p>
             <p>
                 Please contact if you have any special enquiries.
             </p>
-
+            <ul>
+                <li>
+                    <h2>Bristol</h2>
+                    <p>
+                {
+                    ZedPostcodes.bristol.map((el:any,idx)=>{
+                        return(
+                            <span>{el}  </span>
+                        )
+                    })
+                }
+                </p>
+                </li>
+                <li>
+                    <h2>Brighton</h2>
+                    <p>
+                    {
+                    ZedPostcodes.brighton.map((el:any,idx)=>{
+                        return(
+                            <span>{el}  </span>
+                        )
+                    })
+                }
+                    </p>
+                
+                </li>
+                <li>
+                    <h2>London</h2>
+                    <p>
+                    {
+                    ZedPostcodes.hoxton.map((el:any,idx)=>{
+                        return(
+                            <span>{el} </span>
+                        )
+                    })
+                }
+                {
+                    ZedPostcodes.battersea.map((el:any,idx)=>{
+                        return(
+                            <span>{el} </span>
+                        )
+                    })
+                }
+                {ZedPostcodes.waltham_forest.map((el:any,idx)=>{
+                        return(
+                            <span>{el} </span>
+                        )
+                    })}
+                    </p>
+                
+                
+                </li>
+                <li>
+                    <h2>Norwich</h2>
+                    <p>
+                    {
+                        ZedPostcodes.norwich.map((el:any,idx)=>{
+                            return(
+                                <span>{el} </span>
+                            )
+                        })
+                    }
+                    </p>
+                    
+                </li>
+                <li>
+                    <h2>Manchester</h2>
+                    <p>
+                    {
+                        ZedPostcodes.manchester.map((el:any,idx)=>{
+                            return(
+                                <span>{el} </span>
+                            )
+                        })
+                    }
+                    </p>
+                </li>
+                <li>
+                    <h2>Cambridge</h2>
+                    <p>
+                    {
+                        ZedPostcodes.cambridge.map((el:any,idx)=>{
+                            return(
+                                <span>{el} </span>
+                            )
+                        })
+                    }
+                    </p>
+                </li>
+            </ul>
         </section>
         </div>
     )
