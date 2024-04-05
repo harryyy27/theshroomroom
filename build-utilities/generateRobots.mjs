b219d529fe77063d496b738954b5644be6422955
Harry <harryyy27@gmail.com>
2024-04-05 12:00:51 +0100

    robot.txt dynamic

---
+++ build-utilities/generateRobots.mjs
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import * as path from 'path';
+function generateRobotTxt(){
+    var robotText=""
+    if(process.env.NODE_ENV=="production"){
+
+        robotText=`
+        User-agent: *
+
+        Disallow: /auth/*
+        Disallow:/cart
+        Disallow:/myaccount/*
+        Disallow:/checkout*
+        Disallow:/thank-you/$
+        Disallow:*.jpg
+        Disallow:*.png
+        Disallow:*.webp
+        Disallow:*.jpeg
+        
+        Sitemap:${process.env.WEBSITE_NAME}/sitemap.xml
+        `
+    }
+    else{
+        robotText=`
+        User-agent: *
+        Disallow: /
+        `
+    }
+    fs.writeFileSync(path.resolve('./public/robots.txt'),robotText)
+}
+
+generateRobotTxt()
