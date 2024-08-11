4909145263736372d7e4d06f2a6f91dc8f93ef1a
Harry <harryyy27@gmail.com>
2024-08-11 14:36:47 +0100

    meta data added

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -1,6 +1,7 @@
 import {useEffect,useState,useContext} from 'react';
 import Image from 'next/image';
 import {imageMap} from '../../../utils/imageMap/imageMap';
+import productImage from './products/ground_lions_mane_dry.png'
 import {CartContext} from '../../../context/cart';
 import Head from 'next/head';
 import {Metadata} from '../../../utils/metadata/metadata';
@@ -61,6 +62,7 @@ export default function ProductDetails(props:any){
     useEffect(()=>{
         props.setComponentLoading(true)
         setImageUrl(props.urlArr[props.urlArr.length-1].replace(/[\-]/gi,'_').replace('\&apos','').toLowerCase());
+        console.log(props.urlArr[props.urlArr.length-1].replace(/[\-]/gi,'_').replace('\&apos','').toLowerCase())
         setUrl(props.websiteName)
         const initiate = async()=>{
             const session = await getSession()
@@ -171,14 +173,17 @@ export default function ProductDetails(props:any){
             {
                 name&&description&&price?
                 <>
-                <meta property="og:title" content={`Buy ${name} mushrooms UK`}/>
-                <title>Buy {name} mushrooms UK</title>
+                <meta property="og:title" content={`Buy ${name} - UK grown mushrooms | Mega Mushrooms`}/>
+                <title>Buy {name} - UK grown mushrooms | Mega Mushrooms</title>
                 <meta name="description" content={description}/>
+                <meta property="image" content={`${url}/_next/image?url=%2Fproducts%2Fmushrooms%2F${imageUrl.replace('\'',"")}.png&w=3840&q=75`}></meta>
                 <meta property="og:description" content={description}/>
+                <meta property="og:title" content={`Buy ${name} - UK grown mushrooms | Mega Mushrooms`}/>
                 <meta property="og:type" content="product"></meta>
-                <meta property="og:image" content={url}></meta>
-                <meta property="twitter:card" content={url}></meta>
-                <meta property="twitter:description" content={description}></meta>
+                <meta property="og:image" content={`${url}/_next/image?url=%2Fproducts%2Fmushrooms%2F${imageUrl.replace('\'',"")}.png&w=3840&q=75`}></meta>
+                <meta name="twitter:card" content={`${url}/public/products/mushrooms/${imageUrl.replace('\'',"")}.png`}></meta>
+                <meta name="twitter:description" content={description}></meta>
+                <meta name="twitter:title" content={`Buy ${name} - UK grown mushrooms | Mega Mushrooms`}/>
                 <meta property="product:price:amount" content={price}></meta>
                 <meta property="product:price:currency" content="GBP"></meta>
                 <meta property="product:availability" content={stockAvailable!==0?"instock":"oos"}></meta>
@@ -200,7 +205,7 @@ export default function ProductDetails(props:any){
                         url!==''?
                         <>
                         <FacebookShareButton
-                        url={url}
+                        url={'https://megamushroom-test.vercel.app'}
                     >
                         <FacebookIcon size={32} round/>
                     </FacebookShareButton>
